97a500307040821603736e9f04f92c6cf6274a0d
GabrielQuiroz25 <gabrielquiroz@uni.pe>
2024-10-28 20:30:57 +0000

    Agregar test_update_invalidad_data

--- src/test_counter.py
+++ src/test_counter.py
@@ -17,13 +17,13 @@ def test_create_a_counter(client):
     result = client.post("/counters/test_counter")
     
     data = result.get_json()
+
     assert result.status_code == HTTPStatus.CREATED
     assert "test_counter" in data
     assert data["test_counter"] == 0
 
 def test_duplicate_counter(client):
     """Debe devolver un error para duplicados"""
-    # QUITAR ESTE PASS Y DEBUGEAR
     
     result1 = client.post("/counters/test_counter1")
     
@@ -43,3 +43,14 @@ def test_update_a_counter(client):
     data = response.get_json()
     assert response.status_code == HTTPStatus.OK
     assert data["test_counter2"] == 10
+
+def test_update_a_counter_invalid_data(client):
+    """Debe devolver un error para datos inváldos"""
+    # Crear el contador antes de intentar actualizarlo
+    client.post("/counters/test_counter4")
+
+    # Actualizar el contador con un nuevo valor
+    response = client.put("/counters/test_counter4", json={"valor": "abc"})
+    data = response.get_json()
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
