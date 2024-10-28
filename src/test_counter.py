6c7af80a86be6d1d8978982cff9b2611ca5e5482
GabrielQuiroz25 <gabrielquiroz@uni.pe>
2024-10-28 20:15:26 +0000

    Agregar test update_counter

--- src/test_counter.py
+++ src/test_counter.py
@@ -10,19 +10,36 @@ def client():
     # Configuración del cliente de prueba de Flask
     return app.test_client()
 
+  
+
 def test_create_a_counter(client):
     """Debe crear un contador"""
     result = client.post("/counters/test_counter")
-    assert result.status_code == HTTPStatus.CREATED
+    
     data = result.get_json()
+    assert result.status_code == HTTPStatus.CREATED
     assert "test_counter" in data
     assert data["test_counter"] == 0
 
 def test_duplicate_counter(client):
     """Debe devolver un error para duplicados"""
     # QUITAR ESTE PASS Y DEBUGEAR
-    pass
-    #result = client.post("/counters/test_counter")
-    #assert result.status_code == HTTPStatus.CREATED
-    #result = client.post("/counters/test_counter")
-    #assert result.status_code == HTTPStatus.CONFLICT
+    
+    result1 = client.post("/counters/test_counter1")
+    
+    result2 = client.post("/counters/test_counter1")
+    assert result1.status_code == HTTPStatus.CREATED
+    assert result2.status_code == HTTPStatus.CONFLICT
+
+
+def test_update_a_counter(client):
+    """Debe actualizar el valor de un contador existente"""
+    # Crear el contador antes de intentar actualizarlo
+    client.post("/counters/test_counter2")
+
+    # Actualizar el contador con un nuevo valor
+    response = client.put("/counters/test_counter2", json={"valor": 10})
+    
+    data = response.get_json()
+    assert response.status_code == HTTPStatus.OK
+    assert data["test_counter2"] == 10
